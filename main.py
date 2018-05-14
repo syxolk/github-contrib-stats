49e5b9a0048ecff1c34ee99bc6ed21bb7ac27c17
Hans Kirchner <hans-kirchner@gmx.de>
2018-05-14 19:03:56 +0200

    Improve output and simplify counting

--- main.py
+++ main.py
@@ -3,34 +3,22 @@ import argparse
 import requests
 from tqdm import tqdm
 
+
+FORMAT_STRING = "{name: <{fill}}  {author: >6}  {closed: >6}  {pr: >5}"
+
+
 def main():
     args = parse_args()
 
     token = args.token
-    stats = {}
+    stats = Stats()
     print("Get all issues...")
     issues, pull_requests = get_all_issues_and_pr(token, issues_url(args.owner, args.name))
-    for i in tqdm(issues):
-        issue = get_issue(token, i["url"])
 
-        author_stats = stats.get(issue["author"], {"author": 0, "closed": 0, "pr": 0})
-        author_stats["author"] += 1
-        stats[issue["author"]] = author_stats
+    process_issues(token, issues, stats)
+    process_pull_requests(pull_requests, stats)
 
-        closer_stats = stats.get(issue["closed_by"], {"author": 0, "closed": 0, "pr": 0})
-        closer_stats["closed"] += 1
-        stats[issue["closed_by"]] = closer_stats
-
-    for pr in pull_requests:
-        pr_stats = stats.get(pr["user"]["login"], {"author": 0, "closed": 0, "pr": 0})
-        pr_stats["pr"] += 1
-        stats[pr["user"]["login"]] = pr_stats
-
-    for name in stats:
-        print(name)
-        print("\topened: {}".format(stats[name]["author"]))
-        print("\tclosed: {}".format(stats[name]["closed"]))
-        print("\tpr:     {}".format(stats[name]["pr"]))
+    print(stats)
 
 
 def parse_args():
@@ -45,6 +33,7 @@ def issues_url(owner, repo):
     return "https://api.github.com/repos/{owner}/{repo}/issues?state=all&per_page=100".format(
         owner=owner,
         repo=repo)
+
 
 def get_all_issues_and_pr(token, api_url):
     has_more_pages = True
@@ -66,14 +55,64 @@ def get_all_issues_and_pr(token, api_url):
     return (issues, pull_requests)
 
 
-def get_issue(token, api_url):
+def process_issues(token, issues, stats):
+    for i in tqdm(issues):
+        process_single_issue(token, i["url"], stats)
+
+
+def process_single_issue(token, api_url, stats):
     r = requests.get(api_url, headers={
         "Authorization": "token " + token
     }).json()
-    return {
-        "author": r["user"]["login"],
-        "closed_by": r["closed_by"]["login"] if r["closed_by"] is not None else None,
-    }
+
+    stats.count_action(r["user"]["login"], "author")
+    if r["closed_by"] is not None:
+        stats.count_action(r["closed_by"]["login"], "closed")
+
+
+def process_pull_requests(pull_requests, stats):
+    for pr in pull_requests:
+        stats.count_action(pr["user"]["login"], "pr")
+
+
+class Stats:
+    def __init__(self):
+        self._data = {}
+
+    def count_action(self, user, action):
+        if user not in self._data:
+            self._data[user] = {}
+        if action not in self._data[user]:
+            self._data[user][action] = 0
+        self._data[user][action] += 1
+
+    def get_count(self, user, action):
+        if user not in self._data:
+            return 0
+        if action not in self._data[user]:
+            return 0
+        return self._data[user][action]
+
+    def __str__(self):
+        longest_name_len = max((len(x) for x in self._data))
+
+        lines = []
+        lines.append(FORMAT_STRING.format(
+            name="LOGIN",
+            author="OPENED",
+            closed="CLOSED",
+            pr="PR",
+            fill=longest_name_len,
+        ))
+        for name in self._data:
+            lines.append(FORMAT_STRING.format(
+                name=name,
+                author=self.get_count(name, "author"),
+                closed=self.get_count(name, "closed"),
+                pr=self.get_count(name, "pr"),
+                fill=longest_name_len,
+            ))
+        return "\n".join(lines)
 
 
 if __name__ == "__main__":
